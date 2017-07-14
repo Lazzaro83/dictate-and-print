1d2a4f3d40e47ef72aad24b6d5ffe04bb3410581
Lazar Nikolić <casadifianco@gmail.com>
2017-07-14 14:27:05 +0200

    v2

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function record(){
     words.appendChild(p);
 
     recognition.addEventListener('result', e => {
+      console.log(e);
        const transcript = Array.from(e.results)
         .map(result => result[0])
         .map(result => result.transcript)
